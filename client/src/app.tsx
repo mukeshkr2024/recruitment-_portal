8c7f83331f6c230deabd50ff0f8f591900f64201
Mukesh Kumar <Mukeshkumar@cloudprism.in>
2024-08-05 16:18:38 +0530

    add login page

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,43 +1,14 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { HomePage } from "./pages/home"
+import { LoginPage } from "./pages/login"
 
-export function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
-      <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
---
+++ client/src/pages/login.tsx
@@ -0,0 +1,113 @@
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({
+    email: z.string().email(),
+    access_code: z.string().min(4, {
+        message: "Invalid access code"
+    }).max(8, {
+        message: 'Invalid access code'
+    })
+})
+
+export const LoginPage = () => {
+
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            access_code: ""
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+
+    }
+
+    return (
+        <div class="min-h-screen w-full flex items-center justify-center
+bg-[url('/bg_1.png')] bg-cover">
+            <div class="bg-[#FFFFFF0A] border-1 backdrop-blur-3xl px-8 py-6 border-[#FFFFFF2B] w-[458px] rounded-2xl">
+                <div class="flex flex-col gap-y-4">
+                    <div className="mx-auto flex items-center justify-center h-full gap-x-3">
+                        <div>
+                            <img src="/logo_1.svg" alt="Logo" className="w-[80px]" />
+                            <p className="text-[8px] text-[#0D0925] font-medium">CloudPrism Solutions</p>
+                        </div>
+
+                        <div className="h-12 w-[2px] rounded-xl bg-[#0D0925]">
+
+                        </div>
+                        <div className="flex items-center gap-x-2">
+                            <img src="/logo_2.svg" alt="Logo" className="w-[60px]" />
+                            <p className="text-[8px] font-medium text-[#0D0925]">Ridge Consulting <br /> Partner</p>
+                        </div>
+                    </div>
+                    <h2 className="font-semibold text-xl text-[#1A1A1A] mt-4">Welcome</h2>
+                    <div>
+                        <Form {...form}>
+                            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                                <div className="space-y-4">
+
+                                    <FormField
+                                        control={form.control}
+                                        name="email"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel className="font-normal">Login</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="Email" {...field} />
+                                                </FormControl>
+                                                <FormMessage className="text-xs font-normal" />
+                                            </FormItem>
+                                        )}
+                                    />
+
+                                    <FormField
+                                        control={form.control}
+                                        name="access_code"
+                                        render={({ field }) => (
+                                            <FormItem>
+                                                <FormLabel className="font-normal">Enter access code</FormLabel>
+                                                <FormControl>
+                                                    <Input placeholder="Access code" {...field} />
+                                                </FormControl>
+                                                <FormMessage className="text-xs font-normal" />
+                                            </FormItem>
+                                        )}
+                                    />
+                                    <p className="text-sm text-[#007AFF]">Check your email for access code</p>
+                                </div>
+
+                                <div className="">
+                                    <Button type="submit" className="w-full bg-[#5138ED]  hover:bg-[#5138ED]">Get Started</Button>
+                                </div>
+                            </form>
+                        </Form>
+                    </div>
+
+                    <div className="w-full bg-[#E5E5E5] h-[1px]"></div>
+
+                    <div className="flex text-center items-center justify-center gap-x-3 text-sm font-normal">
+                        <p>Didn’t recieved an access?</p>
+                        <span className="text-[#007AFF]">Raise issue</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
