32e5f1a45d0546e7f9bf3b12115364026f968421
Mukesh Kumar <Mukeshkumar@cloudprism.in>
2024-09-23 13:09:57 +0530

    add seed file

--- server/scripts/seed.ts
+++ server/scripts/seed.ts
@@ -1,10 +1,31 @@
-import { applicant } from "../src/db/schema";
+import { eq } from "drizzle-orm";
+import db from "../src/db";
+import { examResult } from "../src/db/schema";
 
 const main = async () => {
     try {
+        const examId = "d34888b3-8b75-4659-852d-0a6bc27439c3";
 
+        const examResults = await db.query.examResult.findMany({
+            where: eq(examResult.examId, examId)
+        });
 
-        console.log("All applicants have been processed.");
+        let totalApplicantsHaving30 = 0;
+        let totalApplicantsHaving35 = 0;
+
+        for (const exam of examResults) {
+            if (exam.score >= 30) {
+                totalApplicantsHaving30++;
+            }
+
+            if (exam.score >= 35) {
+                totalApplicantsHaving35++;
+            }
+        }
+
+        console.log("Total results: " + examResults.length);
+        console.log("Total having score 30 or more: " + totalApplicantsHaving30);
+        console.log("Total having score 35 or more: " + totalApplicantsHaving35);
 
     } catch (error) {
         console.error("Error processing exam results:", error);
