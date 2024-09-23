8db126208d234eb4f255ed853271c99e39aa5ac4
Mukesh Kumar <Mukeshkumar@cloudprism.in>
2024-09-23 19:03:20 +0530

    added  fix

--- server/scripts/seed.ts
+++ server/scripts/seed.ts
@@ -1,4 +1,4 @@
-import { eq } from "drizzle-orm";
+import { eq, desc } from "drizzle-orm";
 import db from "../src/db";
 import { examResult } from "../src/db/schema";
 
@@ -6,26 +6,21 @@ const main = async () => {
     try {
         const examId = "d34888b3-8b75-4659-852d-0a6bc27439c3";
 
-        const examResults = await db.query.examResult.findMany({
-            where: eq(examResult.examId, examId)
+        const topStudents = await db.query.examResult.findMany({
+            where: eq(examResult.examId, examId),
+            orderBy: desc(examResult.score),
+            limit: 50
+        })
+
+        // Extracting IDs of the top 50 students
+        const students = topStudents.map(result => {
+            return {
+                id: result.id,
+                score: result.score,
+            }
         });
 
-        let totalApplicantsHaving30 = 0;
-        let totalApplicantsHaving35 = 0;
-
-        for (const exam of examResults) {
-            if (exam.score > 30) {
-                totalApplicantsHaving30++;
-            }
-
-            if (exam.score > 35) {
-                totalApplicantsHaving35++;
-            }
-        }
-
-        console.log("Total results: " + examResults.length);
-        console.log("Total having score more than 30: " + totalApplicantsHaving30);
-        console.log("Total having score more than 35 " + totalApplicantsHaving35);
+        console.log("Top 50 Students' IDs:", students);
 
     } catch (error) {
         console.error("Error processing exam results:", error);
