c82a28cc97f4b194074ede1e7d967e3757e1c39f
Mukesh Kumar <Mukeshkumar@cloudprism.in>
2024-10-08 16:51:33 +0530

    optimised editor

--- client/src/components/coding-exam/coding-question.tsx
+++ client/src/components/coding-exam/coding-question.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import * as React from "react"
-import { Loader, Play, TriangleAlert, Code2, Terminal } from "lucide-react"
+import { Loader, Play, TriangleAlert, Code2, Terminal, Save, Send, ChevronRight, ChevronLeft, TriangleAlertIcon } from "lucide-react"
 import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from "@/components/ui/resizable"
 import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select"
 import MonacoEditor from "@monaco-editor/react"
@@ -18,7 +18,17 @@ type LanguageOption = {
     runtime?: string
 }
 
-export const CodingQuestion = ({ data }) => {
+type CodingQuestionProps = {
+    data: any;
+    handleNextQuestion: () => void;
+    handlePrevQuestion: () => void;
+    isNextDisabled: boolean;
+    isPrevDisabled: boolean;
+    currentIndex: number;
+    totalQuestions: number;
+}
+
+export const CodingQuestion = ({ data, handleNextQuestion, handlePrevQuestion, isNextDisabled, isPrevDisabled, currentIndex, totalQuestions }: CodingQuestionProps) => {
     const [sourceCode, setSourceCode] = React.useState(codeSnippets["javascript"])
     const [languageOption, setLanguageOption] = React.useState<LanguageOption>(languageOptions[0])
     const [loading, setLoading] = React.useState(false)
@@ -73,24 +83,55 @@ export const CodingQuestion = ({ data }) => {
         }
     }
 
+    const handleSave = () => { }
+
+    const handleSubmit = () => { }
+
     return (
         <Card className="h-full w-full overflow-hidden bg-gradient-to-br from-gray-800 to-gray-900 text-gray-100 shadow-xl">
             <ResizablePanelGroup direction="horizontal" className="w-full h-full rounded-lg">
                 <ResizablePanel defaultSize={40} minSize={30}>
-                    <Card className="h-full bg-gray-700 border-none">
+                    <Card className="h-full bg-gray-700 border-none flex flex-col">
                         <CardHeader className="bg-gray-800 py-3">
-                            <CardTitle className="text-2xl font-bold flex items-center text-blue-300">
-                                <Code2 className="w-6 h-6 mr-2 text-blue-400" />
-                                Question {data?.questionNumber || '01'}
+                            <CardTitle className="text-2xl font-bold flex items-center justify-between text-blue-300">
+                                <div className="flex items-center">
+                                    <Code2 className="w-6 h-6 mr-2 text-blue-400" />
+                                    Question {data?.questionNumber || '01'}
+                                </div>
+                                002
                             </CardTitle>
                         </CardHeader>
-                        <CardContent className="p-6">
+
+                        <CardContent className="p-6 flex-grow">
                             <div
                                 className="prose prose-invert max-w-none text-gray-200"
                                 dangerouslySetInnerHTML={{ __html: data?.questionText }}
                             />
                         </CardContent>
+
+                        <CardFooter className="bg-gray-800 p-4 flex justify-between items-center">
+                            <Button
+                                onClick={handlePrevQuestion}
+                                disabled={isPrevDisabled}
+                                className="bg-gray-700 hover:bg-gray-600 text-white transition-colors px-6 py-2 flex items-center"
+                            >
+                                <ChevronLeft className="w-5 h-5 mr-2" />
+                                Previous
+                            </Button>
+                            <div className="text-gray-300">
+                                Question {currentIndex + 1} of {totalQuestions}
+                            </div>
+                            <Button
+                                onClick={handleNextQuestion}
+                                disabled={isNextDisabled}
+                                className="bg-gray-700 hover:bg-gray-600 text-white transition-colors px-6 py-2 flex items-center"
+                            >
+                                Next
+                                <ChevronRight className="w-5 h-5 ml-2" />
+                            </Button>
+                        </CardFooter>
                     </Card>
+
                 </ResizablePanel>
                 <ResizableHandle withHandle className="bg-gray-600 hover:bg-gray-500 transition-colors" />
                 <ResizablePanel defaultSize={60}>
@@ -102,7 +143,7 @@ export const CodingQuestion = ({ data }) => {
                                         <SelectTrigger className="w-[180px] h-9 bg-gray-700 border-gray-600 text-gray-100">
                                             <SelectValue placeholder="Select a language" />
                                         </SelectTrigger>
-                                        <SelectContent className="bg-gray-700 border-gray-600 text-gray-100">
+                                        <SelectContent className="bg-gray-700 flex-grow border-gray-600 text-gray-100">
                                             <SelectGroup>
                                                 <SelectLabel className="text-gray-400">Languages</SelectLabel>
                                                 {languageOptions.map(option => (
@@ -113,23 +154,35 @@ export const CodingQuestion = ({ data }) => {
                                             </SelectGroup>
                                         </SelectContent>
                                     </Select>
-                                    <Button
-                                        onClick={executeCode}
-                                        disabled={loading}
-                                        className="bg-blue-600 h-9 hover:bg-blue-700 text-white transition-colors"
-                                    >
-                                        {loading ? (
-                                            <>
-                                                <Loader className="w-4 h-4 mr-2 animate-spin" />
-                                                Running...
-                                            </>
-                                        ) : (
-                                            <>
-                                                <Play className="w-4 h-4 mr-2" />
-                                                Run Code
-                                            </>
-                                        )}
-                                    </Button>
+                                    <div className="flex space-x-2">
+
+                                        <Button
+                                            onClick={executeCode}
+                                            disabled={loading}
+                                            className="bg-blue-600 h-9 hover:bg-blue-700 text-white transition-colors"
+                                        >
+                                            {loading ? (
+                                                <>
+                                                    <Loader className="w-4 h-4 mr-2 animate-spin" />
+                                                    Running...
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <Play className="w-4 h-4 mr-2" />
+                                                    Run Code
+                                                </>
+                                            )}
+                                        </Button>
+                                        <Button onClick={handleSave} className="bg-green-600 h-9 hover:bg-green-700 text-white transition-colors">
+                                            <Save className="w-4 h-4 mr-2" />
+                                            Save
+                                        </Button>
+                                        <Button onClick={handleSubmit} className="bg-purple-600 h-9 hover:bg-purple-700 text-white transition-colors">
+                                            <Send className="w-4 h-4 mr-2" />
+                                            Submit
+                                        </Button>
+                                    </div>
+
                                 </CardHeader>
                                 <CardContent className="p-0 h-full">
                                     <MonacoEditor
@@ -171,7 +224,7 @@ export const CodingQuestion = ({ data }) => {
                                                 transition={{ duration: 0.3 }}
                                                 className="flex items-center space-x-2 text-red-300 bg-red-900/20 p-4 rounded-md"
                                             >
-                                                <TriangleAlert className="w-5 h-5 flex-shrink-0" />
+                                                <TriangleAlertIcon className="w-5 h-5 flex-shrink-0" />
                                                 <p>An error occurred. Please check your code and try again.</p>
                                             </motion.div>
                                         ) : output.length > 0 ? (
--- client/src/pages/coding-assessment.tsx
+++ client/src/pages/coding-assessment.tsx
@@ -1,9 +1,11 @@
 import { useGetCodingQuestions } from "@/api/applicants/coding-exam/use-get-coding-questions";
 import { CodingQuestion } from "@/components/coding-exam/coding-question";
+import { useState } from "preact/hooks";
 import { useParams } from "react-router-dom";
 
 export const CodingAssessmentPage = () => {
     const { assesmentId, examId } = useParams();
+    const [currentQuestionIndex, setCurrentQuestionIndex] = useState<number>(0)
 
     const { data, isLoading } = useGetCodingQuestions(assesmentId!, examId!)
 
@@ -12,10 +14,33 @@ export const CodingAssessmentPage = () => {
 
     console.log(assesmentId, examId);
 
+    const handleNextQuestion = () => {
+        if (currentQuestionIndex < (data?.codingQuestions.length || 0) - 1) {
+            setCurrentQuestionIndex((prev) => prev + 1);
+        }
+    };
+
+    const handlePrevQuestion = () => {
+        if (currentQuestionIndex > 0) {
+            setCurrentQuestionIndex((prev) => prev - 1);
+        }
+    };
+
+    const isNextDisabled = currentQuestionIndex >= (data?.codingQuestions.length || 0) - 1;
+
+    const isPrevDisabled = currentQuestionIndex === 0;
+
+
     return (
         <div className="h-screen w-full flex items-center justify-center">
             <CodingQuestion
                 data={data?.codingQuestions[0] || {}}
+                handleNextQuestion={handleNextQuestion}
+                handlePrevQuestion={handlePrevQuestion}
+                isNextDisabled={isNextDisabled}
+                isPrevDisabled={isPrevDisabled}
+                currentIndex={currentQuestionIndex}
+                totalQuestions={data?.codingQuestions.length || 0}
             />
         </div>
     )
